280adf0e097829cd8416d25e6a8dab67b7de0966
Jared Hanson <jaredhanson@gmail.com>
2016-11-30 17:13:26 -0800

    Add exchange.

--- test/package.test.js
+++ test/package.test.js
@@ -6,8 +6,9 @@ var expect = require('chai').expect;
 
 describe('oauth2orize-password-realm', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export exchanges', function() {
+    expect(pkg.exchange).to.be.an('object');
+    expect(pkg.exchange.password).to.be.a('function');
   });
   
 });
